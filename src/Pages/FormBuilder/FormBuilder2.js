d018da4142e71564dec826a7e1d15c24246216ea
devrh <hathi.dev@namasyslimited.onmicrosoft.com>
2022-11-08 19:12:29 +0530

    form generator working

--- src/Pages/FormBuilder/FormBuilder2.js
+++ src/Pages/FormBuilder/FormBuilder2.js
@@ -1,20 +1,26 @@
 import React, { useEffect, useState } from "react";
-import { ReactFormBuilder } from "react-form-builder2";
+import { ReactFormBuilder, ReactFormGenerator } from "react-form-builder2";
 import "react-form-builder2/dist/app.css";
 
 export default function FormBuilder2() {
-  const [form, setForm] = useState();
-
-  useEffect(() => {
-    console.log("hi");
-    console.log(form);
-  }, [form]);
+  const [form, setForm] = useState(null);
+  const [showForm, setShowForm] = useState(false);
 
   //MAKES A POST REQ TO SERVER, SO RUN A SERVER ON /API/FORMDATA, FOR SAVING AND RETRIEVING FORM
 
   return (
     <div>
-      <ReactFormBuilder url="/api/formdata" saveUrl="/api/formdata" />
+      {!showForm && (
+        <ReactFormBuilder
+          onChange={() => console.log("changed!!!")}
+          onSubmit={() => console.log("Submitted!!!")}
+          onPost={(e) => {
+            setForm(e.task_data);
+          }}
+        />
+      )}
+      {showForm && <ReactFormGenerator data={form} />}
+      <button onClick={() => setShowForm((prev) => !prev)}>Show form</button>
     </div>
   );
 }
